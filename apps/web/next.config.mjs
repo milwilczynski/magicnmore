330ad032c6b1b7923bc4471ae48acec53a10ea99
Milosz Wilczynski <milosz.wilczynski@itkek.dev>
2025-03-19 19:53:05 +0100

    svgr build fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
